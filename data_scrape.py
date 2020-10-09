f187e7364715e21f97568524175739fc4f2759a6
Kojo Obeng-Arhin <KwadwoArhin@hotmail.com>
2020-10-09 18:53:23 -0400

    plots + started with ARIMA(3,0,0) model

--- data_scrape.py
+++ data_scrape.py
@@ -7,8 +7,12 @@ Created on Thu Oct  8 19:24:37 2020
 import os
 import requests
 import pandas as pd
+from pandas.plotting import autocorrelation_plot
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
+from statsmodels.tsa.arima_model import ARIMA
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import mean_squared_error
 
 links = ['https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv',
          'https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv']
@@ -17,14 +21,52 @@ links = ['https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/r
 def get_csv(links):
     for link in links:
         r = requests.get(link)
-        with open(str(link).split('/')[8]), 'wb') as f:
+        with open((str(link).split('/')[8]), 'wb') as f:
             f.write(r.content)
+
+
+df = pd.read_csv('confirmed_cases_ontario.csv')
+dff = df.groupby(['Test_Reported_Date'])['Test_Reported_Date'].count()
+
+X = dff.values
+size = int(len(X) * 0.66)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+
+for t in range(len(test)):
+	model = ARIMA(history, order=(3,0,0))
+	model_fit = model.fit(disp=0)
+	output = model_fit.forecast()
+	yhat = output[0]
+	predictions.append(yhat)
+	obs = test[t]
+	history.append(obs)
+	print('predicted=%f, expected=%f' % (yhat, obs))
+
+
+# residuals = pd.DataFrame(model_fit.resid)
+# residuals.plot()
+# residuals.plot(kind='kde')
+# plt.show()
+
+
+#plt.show()
+#residuals.describe()
+
+
+# plot_acf(dff, lags = 50)            
+# plot_pacf(dff)
+# Shows lags 1 and 2 are significant
+
+
+
 
 if __name__ == '__main__':
     get_csv(links)
 
 '''
-df = pd.read_csv('confirmed_cases_ontario.csv')
+
 
 df.info()
 
