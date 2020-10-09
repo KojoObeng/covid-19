1adaba6a7a7973b063bc81fb6580f0992d2379d0
Jason Huynh <jason.huynh@ryerson.ca>
2020-10-08 20:38:45 -0400

    updated python script

--- data_scrape.py
+++ data_scrape.py
@@ -4,15 +4,15 @@ Created on Thu Oct  8 19:24:37 2020
 
 @author: Jason
 """
-
-import pandas as pd
+import os
 import requests
 
 
-url = 'https://data.ontario.ca/dataset/'
-data_set = ['status-of-covid-19-cases-in-ontario', 
-            'confirmed-positive-cases-of-covid-19-in-ontario']
+os.chdir('C:\\Users\\User\\Documents\\School\\code\\COVID-19\\covid-19')
 
+url = 'https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv'
 
-def fetch_data(url):
-    
+r = requests.get(url)
+with open('confirmed_cases_ontario.csv', 'wb') as f:
+    f.write(r.content) 
+
